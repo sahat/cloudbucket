e2860af7734b84f627a27fcf1e00c74c7192537e
Sahat Yalkabov <sakhat@gmail.com>
2013-04-22 00:07:10 -0400

    added facebook controller and dialog box view

--- server/public/app/controller/Facebook.js
+++ server/public/app/controller/Facebook.js
@@ -1,17 +1,27 @@
 Ext.define('Semantica.controller.Facebook', {
-    extend: 'Ext.app.Controller',
-    
-    config: {
-        refs: {
-            
-        },
-        control: {
-            
-        }
-    },
-    
-    //called when the Application is launched, remove if not needed
-    launch: function(app) {
-        
+  extend: 'Ext.app.Controller',
+
+  config: {
+    control: {
+      '#fbLogin': {
+        tap: 'onFacebookLogin'
+      }
     }
+  },
+
+  init: function() {
+    Semantica.Facebook.on({
+      exception: function() {
+        Ext.create('Semantica.view.Dialog', { msg: 'The connection to Facebook has timed out' }).show();
+      },
+      loginStatus: function() {
+        Ext.get('loading').destroy();
+      }
+    });
+  },
+
+  // Redirect to Facebook when the user taps the Facebook Login button
+  onFacebookLogin: function() {
+    window.top.location = WL.Facebook.redirectUrl();
+  }
 });
---
+++ server/public/app/view/Dialog.js
@@ -0,0 +1,62 @@
+Ext.define('WL.view.Dialog', {
+
+  extend: 'Ext.Sheet',
+
+  config: {
+
+    layout: 'vbox',
+    cls: 'dialog',
+
+    showAnimation: {
+      type: 'popIn',
+      duration: 250,
+      easing: 'ease-out'
+    },
+
+    hideAnimation: {
+      type: 'popOut',
+      duration: 250,
+      easing: 'ease-out'
+    },
+
+    zIndex: 10,
+
+    msg: '',
+    buttons: []
+  },
+
+  show: function() {
+    if (!this.getParent() && Ext.Viewport) {
+      Ext.Viewport.add(this);
+    }
+    this.callParent();
+  },
+
+  initialize: function() {
+
+    this.callParent();
+
+    this.insert(0, {
+      xtype: 'button',
+      ui: 'close',
+      handler: function() {
+        this.hide()
+      },
+      scope: this
+    });
+
+    this.insert(1, {
+      xtype: 'component',
+      cls: 'msg',
+      html: this.getMsg()
+    });
+
+    if (this.getButtons().length) {
+      Ext.Array.each(this.getButtons(), function(btn) {
+        this.add(Ext.applyIf(btn, {
+          xtype: 'button'
+        }));
+      }, this);
+    }
+  }
+});
