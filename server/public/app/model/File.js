db8b022781191425a6c66a77fe46006c41c0927b
Sahat Yalkabov <sakhat@gmail.com>
2013-04-21 22:38:02 -0400

    updated sencha FILES model

--- server/public/app/model/File.js
+++ server/public/app/model/File.js
@@ -1,11 +1,21 @@
 Ext.define('Semantica.model.File', {
-    extend: 'Ext.data.Model',
-    
-    config: {
-        fields: [
-            { name: 'name', type: 'auto' },
-            { name: 'modified', type: 'auto' },
-            { name: 'id', type: 'auto' }
-        ]
+  extend: 'Ext.data.Model',
+
+  config: {
+    fields: [
+      '_id',
+      'name',
+      'filetype',
+      'size',
+      'path',
+      'lastAccessed',
+      'lastModified',
+      'keywords',
+      'summary'
+    ],
+    proxy: {
+      type: 'rest',
+      url: '/files'
     }
+  }
 });
