d0abe3881c81441727fe4abe6d81bf7e342cbe49
Sahat Yalkabov <sakhat@gmail.com>
2013-07-18 01:47:38 -0400

    improved navbar icons, further optimizations to slide-in menu

--- public/js/app.js
+++ public/js/app.js
@@ -12,15 +12,31 @@ $(document).ready(function() {
     mainClass: 'my-mfp-zoom-in'
   });
 
-  $('.menu-popup-button').magnificPopup({
-    type: 'inline',
-    fixedContentPos: false,
-    fixedBgPos: true,
-    preloader: false,
-    overflowY: 'auto',
-    showCloseBtn: false,
-    removalDelay: 300,
-    mainClass: 'mfp-move-from-top'
+  // $('.menu-popup-button').magnificPopup({
+  //   type: 'inline',
+    
+  // });
+  $('.menu-popup-button').click(function() {
+    
+    if (!$('#menu').hasClass('mfp-hide')) {
+      return $.magnificPopup.close();
+    }
+    
+    $.magnificPopup.open({
+      items: {
+        src: $('#menu'),
+        type: 'inline'
+      },
+      fixedContentPos: true,
+      fixedBgPos: true,
+      preloader: false,
+      overflowY: 'hidden',
+      showCloseBtn: false,
+      removalDelay: 300,
+      mainClass: 'mfp-move-from-top'
+    });
   });
+  
+
   
 });
