914708271ff0ea82ddf56e439544a9dbc0aee137
Sahat Yalkabov <sakhat@gmail.com>
2013-09-11 12:10:37 -0400

    removed old backbone templates, schema changes

--- schema.js
+++ schema.js
@@ -17,14 +17,12 @@ var File = new mongoose.Schema({
   lastModified: Date,
   isFolder: Boolean,
 
-
   keywords: Array,
   category: String,
   concepts: Array,
   entities: Array,
   summary: String,
   user: String,
-
 
   genre: Array,
   title: String,
@@ -41,12 +39,7 @@ exports.File = File;
 
 // User schema
 exports.User = mongoose.model('User', new mongoose.Schema({
-  googleId: {
-    type: String,
-    index: {
-      unique: true
-    }
-  },
+  googleId: { type: String, index: { unique: true } },
   accessToken: String,
   displayName: String,
   link: String,
