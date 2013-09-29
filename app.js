b3133ae759ad64ecbac9bd13a986a6da998784ca
Sahat Yalkabov <sakhat@gmail.com>
2013-09-28 20:37:37 -0400

    added elasticity on iscroller, added fadein fadeout effect on search-header

--- app.js
+++ app.js
@@ -108,8 +108,8 @@ passport.deserializeUser(function(googleId, done) {
 passport.use(new GoogleStrategy({
     clientID: config.GOOGLE_CLIENT_ID,
     clientSecret: config.GOOGLE_CLIENT_SECRET,
-    //callbackURL: "http://localhost:3000/auth/google/callback"
-    callbackURL: "http://cloudbucket.sahat.c9.io/auth/google/callback"
+    callbackURL: "http://localhost:3000/auth/google/callback"
+    //callbackURL: "http://cloudbucket.sahat.c9.io/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
