b11268f02a8251163b44b906704f41c22ffe0775
Sahat Yalkabov <sakhat@gmail.com>
2013-09-29 04:12:08 -0400

    updated specrunner for mocha

--- public/js/test/specs/index-test.js
+++ public/js/test/specs/index-test.js
@@ -1,10 +1,10 @@
 define(['index'], function() {
   var models = require('models');
 
-  describe('Models', function() {
+  describe('Index Route', function() {
 
-    describe('Sample Model', function() {
-      it('should default "urlRoot" property to "/api/samples"', function() {
+    describe('Hammer.js', function() {
+      it('should be initialized on "#scroller li"', function() {
         var sample = new models.Sample();
         sample.urlRoot.should.equal('/api/samples');
       });
