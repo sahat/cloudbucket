76dd46fba0adee2b0e9df71b3e44550b9664b434
Sahat Yalkabov <sakhat@gmail.com>
2013-09-29 15:37:18 -0400

    fadeout animation on file remove using hammer.js hold

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
-define(['vex', 'vexDialog', 'hammer'], function(vex, vexDialog) {
+define(['jquery', 'vex', 'vexDialog', 'hammer', 'animateCSS'], function($, vex, vexDialog) {
   console.log('Loading index.');
 
   var hammertime = $('#scroller li').hammer();
 
   hammertime.on('hold', function(ev) {
 
+    var target = $(ev.currentTarget);
     var id = $(ev.currentTarget).data('id');
 
     vex.defaultOptions.className = 'vex-theme-os';
@@ -12,11 +13,20 @@ define(['vex', 'vexDialog', 'hammer'], function(vex, vexDialog) {
       message: 'Are you sure you want to delete this file?',
       callback: function(value) {
         if (value) {
+
+          // remove from DOM
+          target.animateCSS('fadeOutDown', function() {
+            target.hide();
+          });
+
           $.ajax({
             type: 'DELETE',
             url: '/files/' + id,
             success: function() {
               console.log('File has been deleted');
+
+
+
             },
             error: function(xhr, status, error) {
               console.log(status, error);
