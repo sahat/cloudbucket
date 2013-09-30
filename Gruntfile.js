1f53807b4f8015feef231e899e7caa14c7773710
Sahat Yalkabov <sakhat@gmail.com>
2013-09-29 23:16:19 -0400

    RequiredJS task in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,53 @@
-/**
- * Created by Sahat on 9/27/13.
- */
+module.exports = function(grunt) {
+  grunt.initConfig({
+//    less: {
+//      development: {
+//        options: {
+//          concat: false
+//        },
+//        files: [
+//          {
+//            expand: true,
+//            concat: false,
+//            cwd: 'src/less',
+//            src: ['*.less'],
+//            dest: 'build/css',
+//            ext: '.css'
+//          }
+//        ]
+//      }
+//    },
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: 'public/js',
+          name: 'main',
+          dir: 'release',
+          mainConfigFile: 'public/js/main.js',
+          fileExclusionRegExp: /^test$/
+        }
+      }
+    }
+//    watch: {
+//      scripts: {
+//        files: ['src/coffee/**/*.coffee'],
+//        tasks: ['coffee'],
+//        options: {
+//          spawn: false
+//        }
+//      },
+//      styles: {
+//        files: ['src/less/**/*.less'],
+//        tasks: ['less'],
+//        options: {
+//          spawn: false
+//        }
+//      }
+//    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['requirejs']);
+};
