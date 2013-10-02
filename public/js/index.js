92227ccca9a2ee885eed793b97c96e0f94c20ce5
Sahat Yalkabov <sakhat@gmail.com>
2013-10-01 22:28:21 -0400

    Switched to original nice upload form, also instead of full fake path it only displays the file name

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,21 @@
 define(['jquery', 'vex', 'vexDialog', 'hammer', 'animateCSS'], function($, vex, vexDialog) {
-  console.log('Loading index.');
+  console.log('loading index.');
+
+  $('#browse').click(function() {
+    console.log('click');
+    $('input[id=userFile]').click();
+  });
+
+  $('input[id=userFile]').change(function() {
+    var path = $(this).val();
+    console.log(path)
+    var fileName = path.split('\\') || path.split('/');
+    console.log(fileName);
+    if (fileName.length > 0) {
+      $('#uploadField').val(fileName.slice(-1)[0]);
+    } else {
+      $('#uploadField').val(path);
+    }
+
+  });
 });
