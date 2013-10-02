7e5db08d548361f96b25b83353a506a08017bcc3
Sahat Yalkabov <sakhat@gmail.com>
2013-10-02 10:00:07 -0400

    wrapped a href link around li on index.jade

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,12 @@ define(['jquery', 'vex', 'vexDialog', 'hammer', 'animateCSS'], function($, vex, 
 
   hammertime.on('dragstart', function(ev) {
     ev.gesture.stopDetect(); // stop release event firing
+    ev.preventDefault();
     $(this).removeClass('selected');
   });
 
-  hammertime.on('release', function(ev) {
-    $(this).removeClass('selected');
-    $(this).find('a')[0].click();
-  });
+//  hammertime.on('release', function(ev) {
+//    $(this).removeClass('selected');
+//    $(this).find('a')[0].click();
+//  });
 });
