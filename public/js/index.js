7f5a3ccd910d5ec62f80e193789033963af1dd83
Sahat Yalkabov <sakhat@gmail.com>
2013-10-10 01:21:04 -0400

    addedtouch events in addition to click events when trying to open a file

--- public/js/index.js
+++ public/js/index.js
@@ -18,6 +18,11 @@ define(['jquery', 'vex', 'vexDialog', 'humane', 'hammer', 'animateCSS'], functio
   hammertime.on('touch', function(ev) {
     $(this).addClass('selected');
   });
+  hammertime.on('release', function(ev) {
+    $(this).find('.file-loader').show();
+    $(this).removeClass('selected');
+    $(this).find('a')[0].click();
+  });
 
   hammertime.on('dragstart', function(ev) {
     ev.gesture.stopDetect(); // stop release event firing
