e1ea9cde3de4e79c446b010493a7a64b42d0fe6b
Sahat Yalkabov <sakhat@gmail.com>
2013-10-26 01:00:31 -0400

    minified, switched to openshift

--- server.js
+++ server.js
@@ -119,8 +119,8 @@ passport.deserializeUser(function(googleId, done) {
 passport.use(new GoogleStrategy({
     clientID: config.GOOGLE_CLIENT_ID,
     clientSecret: config.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
-    //callbackURL: "http://cloudbucket-sahat.rhcloud.com/auth/google/callback"
+    //callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "http://cloudbucket-sahat.rhcloud.com/auth/google/callback"
     //callbackURL: "http://cloudbucket.sahat.c9.io/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
