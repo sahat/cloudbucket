eeb83372b76431b5505de17c1b9520991403570e
Sahat Yalkabov <sakhat@gmail.com>
2013-11-28 16:36:58 -0500

    fixed the icon spinner from not showing up properly (different classes in FA 4.0)

--- public/js/index.js
+++ public/js/index.js
@@ -9,11 +9,13 @@ define(['jquery', 'vex', 'vexDialog', 'humane', 'hammer', 'animateCSS'], functio
   var hammertime = $(".inner").hammer();
 
   hammertime.on('release', function(ev) {
+    console.log('touch released');
     $(this).find('.file-loader').show();
     $(this).find('a')[0].click();
   });
 
   hammertime.on('dragstart', function(ev) {
+    console.log('starting drag event');
     ev.gesture.stopDetect(); // stop release event firing
     ev.preventDefault();
   });
