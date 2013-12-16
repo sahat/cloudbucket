ee61cc81b7c542218a6062d87a82c8a1d50e2d29
Sahat Yalkabov <sakhat@gmail.com>
2013-12-16 04:45:31 -0500

    google callback route refactoring

--- server.js
+++ server.js
@@ -255,16 +255,9 @@ app.get('/auth/google', passport.authenticate('google', {
 
 /**
  * GET /auth/google/callback
- * Use passport.authenticate() as route middleware to authenticate the
- * request. If authentication fails, the user will be redirected back to the
- * login page. Otherwise, the primary route function function will be called,
- * which, in this example, will redirect the user to the home page.
  */
-app.get('/auth/google/callback', passport.authenticate('google', {
-  failureRedirect: '/login'
-}), function (req, res) {
-  res.redirect('/');
-});
+app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
+
 
 
 
